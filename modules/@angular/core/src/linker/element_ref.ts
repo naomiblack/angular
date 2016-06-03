b21e6ad9d1b96f3f5c052e059c7bdd58ea348af8
Naomi Black <naomitraveller@gmail.com>
2016-06-02 19:16:11 -0700

    chore(docs): WIP example of security and new docs tags

--- modules/@angular/core/src/linker/element_ref.ts
+++ modules/@angular/core/src/linker/element_ref.ts
@@ -1,8 +1,13 @@
 /**
- * A wrapper around a native element inside of a View.
+ * @whatItDoes A wrapper around a native element inside of a View.
  *
- * An `ElementRef` is backed by a render-specific element. In the browser, this is usually a DOM
- * element.
+ * @security Granting direct access to the DOM may make you vulnerable to XSS attacks. For
+ * See the [Security Guide](../guide/security.html).
+ * 
+ * @howToUse An `ElementRef` is backed by a render-specific element. In the browser, this is
+ * usually a DOM element.
+ *
+ * @stable
  */
 // Note: We don't expose things like `Injector`, `ViewContainer`, ... here,
 // i.e. users have to ask for what they need. With that, we can build better analysis tools
